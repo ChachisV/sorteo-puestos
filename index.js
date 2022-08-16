11189b85ed588e72bd15e428332e810f36e7974e
Jaime Andres Vanegas <javanmot@gmail.com>
2022-08-15 23:16:01 -0500

    se crearon funciones para mejorar el codigo

--- index.js
+++ index.js
@@ -15,36 +15,39 @@ function aleatorio(min, max){
     return Math.floor(Math.random()*(max-min + 1)+min);
 }
 
+function seleccionarModulo(){
 
-
-function escogerModulo(){
     let modulo = aleatorio(min, max);
     let consultor = document.querySelector(".input-consultor").value;
-    
+
+
+    if(!modulos.includes(modulo)){     
+      
+        modulos.push(modulo); 
+        
+        const resultadoModulo = document.createElement("p");
+        resultadoModulo.innerText = (consultor + " en el módulo ");
+ 
+        const moduloElegido = document.createElement("p");
+        moduloElegido.classList.add("modulo-final");
+        moduloElegido.innerText = (modulo)
+ 
+        moduloFinal.append(resultadoModulo, moduloElegido);
+         
+        
+                      
+     }else{
+         seleccionarModulo(consultor);
+        }
+}
+
+function modulosLlenos(){
+        
     if(modulos.length >= max){
         alert("ya están ocupados todos los módulos");
     }else {
-        if(!modulos.includes(modulo)){     
-      
-            modulos.push(modulo); 
-            
-            const resultadoModulo = document.createElement("p");
-            resultadoModulo.innerText = (consultor + " en el módulo ");
-     
-            const moduloElegido = document.createElement("p");
-            moduloElegido.classList.add("modulo-final");
-            moduloElegido.innerText = (modulo)
-     
-            moduloFinal.append(resultadoModulo, moduloElegido);
-            
-                          
-         }else{
-             escogerModulo(consultor);
-         }
+        seleccionarModulo()
     }
-
-  
-      
 }
 
 
@@ -59,15 +62,20 @@ function eleccionModulos(){
 
     const inputConsultor = document.createElement("input");
     inputConsultor.classList.add("input-consultor");
-    inputConsultor.setAttribute("type", "text")
+    inputConsultor.setAttribute("type", "text");
+    inputConsultor.setAttribute("placeholder", "Nombre del consultor")
+    
 
     const botonElegirModulo = document.createElement("button");
     botonElegirModulo.innerText = "Elegir Módulo";
+    botonElegirModulo.setAttribute("type", "submit")
     botonElegirModulo.classList.add("boton-elegir-modulo");
-    botonElegirModulo.addEventListener("click", escogerModulo);
+    botonElegirModulo.addEventListener("click",  modulosLlenos);
+    
     
 
-    seccionConsultores.append(nombreConsultor, inputConsultor, botonElegirModulo);
+    seccionConsultores.append(nombreConsultor, inputConsultor,botonElegirModulo );
+    
 
     
     
